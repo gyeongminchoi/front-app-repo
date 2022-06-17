417295551f8acc9e88994cb2d679fd864ad3b1b1
gyeongminchoi <fkauskm@nate.com>
2022-06-17 03:54:05 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 202206171300
+            EKS DEMO Blog version 202206171310
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
