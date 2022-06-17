ae4ba46f014d8b32079c10f656cb150a1f659c30
gyeongminchoi <fkauskm@nate.com>
2022-06-17 01:57:08 +0000

    Add kustomize image edit

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version 202206171045
+            EKS DEMO Blog version 202206171100
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
